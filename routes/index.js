dc3715b3c02019b3701004731d3960e8c76c001f
Narek <n-ginosyan@mail.ru>
2023-08-15 11:48:12 +0400

    a

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import users from './users';
 
 const router = express.Router();
-//aa
 
 /* GET home page. */
 router.get('/', (req, res, next) => { // eslint-disable-line no-unused-vars
