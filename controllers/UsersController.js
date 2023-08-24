130747a9569f906ae6a29049369cf45108ba9d9e
Hovhannes Galstyan <galstyaannn@gmail.com>
2023-08-24 11:17:56 +0400

    test

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,17 +8,7 @@ import Users from '../models/Users';
 import Mail from '../services/Mail';
 
 const { JWT_SECRET } = process.env;
-
 class UsersController {
-  static register2 = async (req, res, next) => {
-    try {
-      const { firstName } = req.body;
-      console.log(firstName);
-    } catch (e) {
-      next(e);
-    }
-  };
-
   static register = async (req, res, next) => {
     try {
       console.log(req.body);
