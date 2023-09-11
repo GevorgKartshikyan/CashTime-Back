71e50d75fe1e8de4ab2f09708b02f164f8cb5296
GevorgKartshikyan <karchikyan.00@mail.ru>
2023-09-11 22:05:28 +0400

    update editJob static on JobsController

--- controllers/JobsController.js
+++ controllers/JobsController.js
@@ -167,6 +167,75 @@ class JobsController {
     }
   };
 
+  static editJob = async (req, res, next) => {
+    try {
+      const { jobId } = req.params;
+      const { file } = req;
+      const body = JSON.parse(req.body.data);
+      const {
+        dataFromChild1: title,
+        dataFromChild2: skills,
+        dataFromChild3: experience,
+        dataFromChild4: price,
+        dataFromChild5: description,
+        dataFromChild6,
+      } = body;
+      const { address, phoneNumber } = dataFromChild6;
+      let location = null;
+
+      if (address && address.longitude && address.latitude) {
+        location = {
+          type: 'Point',
+          coordinates: [address.longitude, address.latitude],
+        };
+      }
+
+      let jobPhoto;
+      if (file) {
+        jobPhoto = path.join(`/images/jobs/${uuidV4()}_${file.originalname}`);
+        const filePath = path.resolve(path.join('public', jobPhoto));
+        fs.writeFileSync(filePath, file.buffer);
+      }
+
+      const { city, country, fullAddress } = address;
+
+      const updatedJob = {
+        title,
+        skills,
+        experience,
+        priceMethod: price.method,
+        priceFixed: price.maxPrice,
+        priceMinHourly: price.priceFrom,
+        priceMaxHourly: price.priceTo,
+        description,
+        geometry: location,
+        phoneNumber,
+        city,
+        country,
+        fullAddress,
+        jobPhoto,
+        status: 'pending',
+      };
+
+      const [rowsAffected, [updatedJobData]] = await Jobs.update(updatedJob, {
+        where: { id: jobId },
+        returning: true,
+      });
+
+      if (rowsAffected === 0) {
+        throw HttpError(404, 'job not found');
+      }
+
+      res.json({
+        job: updatedJobData,
+        status: 'ok',
+      });
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  };
+
   static activateJob = async (req, res, next) => {
     try {
       const { jobId } = req.body;
@@ -280,71 +349,6 @@ class JobsController {
       job.alreadyDone = true;
       await job.save();
     } catch (e) {
-      next(e);
-    }
-  };
-
-  static editJob = async (req, res, next) => {
-    try {
-      const { jobId } = req.params;
-      const { file } = req;
-      const body = JSON.parse(req.body.data);
-      const {
-        dataFromChild1: title,
-        dataFromChild2: skills,
-        dataFromChild3: experience,
-        dataFromChild4: price,
-        dataFromChild5: description,
-        dataFromChild6,
-      } = body;
-      const { address, phoneNumber } = dataFromChild6;
-      let location = null;
-
-      if (address && address.longitude && address.latitude) {
-        location = {
-          type: 'Point',
-          coordinates: [address.longitude, address.latitude],
-        };
-      }
-
-      let jobPhoto;
-      if (file) {
-        jobPhoto = path.join(`/images/jobs/${uuidV4()}_${file.originalname}`);
-        const filePath = path.resolve(path.join('public', jobPhoto));
-        fs.writeFileSync(filePath, file.buffer);
-      }
-
-      const { city, country, fullAddress } = address;
-
-      const updatedJob = {
-        title,
-        skills,
-        experience,
-        price,
-        description,
-        geometry: location,
-        phoneNumber,
-        city,
-        country,
-        fullAddress,
-        jobPhoto: jobPhoto || undefined,
-      };
-
-      const [rowsAffected, [updatedJobData]] = await Jobs.update(updatedJob, {
-        where: { id: jobId },
-        returning: true,
-      });
-
-      if (rowsAffected === 0) {
-        throw HttpError(404, 'job not found');
-      }
-
-      res.json({
-        job: updatedJobData,
-        status: 'ok',
-      });
-    } catch (e) {
-      console.error(e);
       next(e);
     }
   };
