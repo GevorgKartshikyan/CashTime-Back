97ddde0d84032af01c027715f34b17960852464e
Narek <n-ginosyan@mail.ru>
2023-10-11 11:39:08 +0400

    a

---
+++ models/Files.js
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../services/sequelize';
+import Messages from './Messages';
+
+class Files extends Model {
+
+}
+
+Files.init({
+  id: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  path: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  size: {
+    type: DataTypes.BIGINT.UNSIGNED,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'files',
+  tableName: 'files',
+});
+
+Files.belongsTo(Messages, {
+  foreignKey: 'messageId',
+  as: 'files',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+Messages.hasMany(Files, {
+  foreignKey: 'messageId',
+  as: 'files',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+Messages.hasOne(Files, {
+  foreignKey: 'messageId',
+  as: 'file',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+export default Files;
--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import multer from 'multer';
+// import { v4 as uuidV4 } from 'uuid';
+import HttpError from 'http-errors';
 import MessagesController from '../controllers/MessagesController';
 
 const router = express.Router();
 
-router.post('/send', MessagesController.send);
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     filename(req, file, cb) {
+//       cb(null, `${uuidV4()}__${file.originalname}`);
+//     },
+//   }),
+// });
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+  fileFilter: (req, file, cb) => {
+    if (['image/png', 'image/jpeg', 'image/webp', 'image/gif', 'image/svg+xml', 'image/bmp', 'image/tiff', 'image/x-icon'].includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(HttpError(422, 'Invalid file type'), false);
+    }
+  },
+});
+
+router.post('/send', upload.array('files[]', 12), MessagesController.send);
 router.get('/list', MessagesController.list);
 router.put('/open', MessagesController.open);
 router.put('/typing', MessagesController.friendTyping);
